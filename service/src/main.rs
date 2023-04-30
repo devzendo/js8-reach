bd854d4df69d13439f5293a16ac164f24acdb4e8
Matt Gumbley <matt.gumbley@devzendo.org>
2023-04-30 21:30:07 +0100

    Hello world

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] 
+extern crate rocket;
+extern crate log;
+
+use std::env;
+
+
+#[get("/")]
+fn world() -> &'static str {
+    "Hello, world!"
 }
+
+#[launch]
+fn rocket() -> _ {
+    match env::var("RUST_LOG") {
+        Ok(_) => {}
+        Err(_) => env::set_var("RUST_LOG", "info")
+    }
+    env_logger::init();
+
+    rocket::build()
+        .mount("/", routes![world])
+}
+
