352adfe45db5359bcb8c238eca13494a317324a7
Matt Gumbley <matt.gumbley@devzendo.org>
2023-05-02 21:38:32 +0100

    Build the yew front end, serve it as static files via rocket. Rebuild front end with trunk when running rocket.

--- service/src/main.rs
+++ service/src/main.rs
@@ -3,11 +3,31 @@ extern crate rocket;
 extern crate log;
 
 use std::env;
+use rocket::fs::NamedFile;
+use rocket::response::status::NotFound;
+use std::path::PathBuf;
 
+// Return the index file as a Rocket NamedFile
+async fn get_index() -> Result<NamedFile, NotFound<String>> {
+    NamedFile::open("../ui/dist/index.html")
+        .await
+        .map_err(|e| NotFound(e.to_string()))
+}
 
+//Create a route for any url that is a path from the /
+#[get("/<path..>")]
+async fn static_files(path: PathBuf) -> Result<NamedFile, NotFound<String>> {
+    let path = PathBuf::from("../ui/dist").join(path);
+    match NamedFile::open(path).await {
+        Ok(f) => Ok(f),
+        Err(_) => get_index().await,
+    }
+}
+
+// Return the index when the url is /
 #[get("/")]
-fn world() -> &'static str {
-    "Hello, world!"
+async fn index() -> Result<NamedFile, NotFound<String>> {
+    get_index().await
 }
 
 #[launch]
@@ -18,7 +38,8 @@ fn rocket() -> _ {
     }
     env_logger::init();
 
+    // You must mount the static_files route
     rocket::build()
-        .mount("/", routes![world])
+        .mount("/", routes![index, static_files])
 }
 
